745a1bf45eeb111f4c986e69d4e5adc2aff0d316
Polonkai Pál <polonkaipal5@gmail.com>
2021-11-02 10:03:53 +0100

    Tábla inicializálása

--- flip.py
+++ flip.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 import sys
+import csv
+
+tabla = []
+
+def tabla_ini(ini_fajl):
+    with open(ini_fajl, 'r') as f:
+        r = csv.reader(f)
+        for s in r:
+            tabla.append(s)
 
 def main():
     print("Flip")
@@ -12,17 +21,20 @@ def main():
         return
 
     # Argumentumok változókhoz rendelése
-    elolap   = sys.argv[1]
-    hatlap   = sys.argv[2]
-    cellap   = sys.argv[3]
-    sor      = sys.argv[4]
-    oszlop   = sys.argv[5]
+    sor      = sys.argv[1]
+    oszlop   = sys.argv[2]
+    elolap   = sys.argv[3]
+    hatlap   = sys.argv[4]
+    cellap   = sys.argv[5]
     ini_fajl = sys.argv[6]
 
     # Előfeltétel vizsgálat
     if cellap != elolap and cellap != hatlap:
         print("None")
         return
+
+    # Tábla inicializálása
+    tabla_ini(ini_fajl)
 
 if __name__ == "__main__":
     main()
