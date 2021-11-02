79fc5e6d82e445c6f1e72d766ff30ab7e0839395
Polonkai Pál <polonkaipal5@gmail.com>
2021-11-02 09:27:52 +0100

    Kommentek hozzáadása

--- flip.py
+++ flip.py
@@ -5,11 +5,13 @@ import sys
 def main():
     print("Flip")
 
+    # Argumentumok ellenőrzése
     if len(sys.argv) != 7:
     # Azért 7, mert a 0. indexű elemben a most futó fájl neve található, így 6 + 1 = 7
         print("Nem megfelelő számú argumentumot adtál meg!")
         return
 
+    # Argumentumok változókhoz rendelése
     elolap   = sys.argv[1]
     hatlap   = sys.argv[2]
     cellap   = sys.argv[3]
@@ -17,6 +19,7 @@ def main():
     oszlop   = sys.argv[5]
     ini_fajl = sys.argv[6]
 
+    # Előfeltétel vizsgálat
     if cellap != elolap and cellap != hatlap:
         print("None")
         return
