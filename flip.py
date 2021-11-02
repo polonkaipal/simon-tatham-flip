2c562a6001f4581f41571a813a545d2388500a8d
Polonkai Pál <polonkaipal5@gmail.com>
2021-11-02 09:17:52 +0100

    Parancssori argumentum kezelése

--- flip.py
+++ flip.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 
+import sys
+
 def main():
     print("Flip")
+
+    if len(sys.argv) != 7:
+    # Azért 7, mert a 0. indexű elemben a most futó fájl neve található, így 6 + 1 = 7
+        print("Nem megfelelő számú argumentumot adtál meg!")
+        return
+
+    elolap   = sys.argv[1]
+    hatlap   = sys.argv[2]
+    cellap   = sys.argv[3]
+    sor      = sys.argv[4]
+    oszlop   = sys.argv[5]
+    ini_fajl = sys.argv[6]
+
+    if cellap != elolap and cellap != hatlap:
+        print("None")
+        return
 
 if __name__ == "__main__":
     main()
